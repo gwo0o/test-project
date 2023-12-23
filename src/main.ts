0e33cfd06212c0124660ecb364e0aba44c11ab32
gwo0o <102335174+gwo0o@users.noreply.github.com>
2023-12-24 05:26:55 +0900

    eslint, prettier 적용

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const options = new DocumentBuilder()
+    .setTitle('NestJS Study API Docs')
+    .setDescription('NestJS Study API description')
+    .setVersion('1.0.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  SwaggerModule.setup('docs', app, document);
+
+  await app.listen(3030);
 }
 bootstrap();
